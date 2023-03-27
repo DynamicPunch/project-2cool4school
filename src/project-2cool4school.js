843eeb5ea4eadbed70e7fd92749b1047f36e5f2d
DynamicPunch <michaelgardner231@gmail.com>
2023-03-27 00:13:43 -0400

    test

--- src/project-2cool4school.js
+++ src/project-2cool4school.js
@@ -4,83 +4,123 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class Project2cool4school extends LitElement {
   static properties = {
-    header: { type: String },
-  }
+    badgeTopTitle: {
+      type: String,
+      reflect: true,
+    },
+
+    badgePic: {
+      type: String,
+    },
+
+    badgeTitleName: {
+      type: String,
+      reflect: true,
+    },
+
+    badgeCreatorName: {
+      type: String,
+      reflect: true,
+    },
+  };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--project-2cool4school-background-color);
-    }
-
     main {
-      flex-grow: 1;
+      padding: 20px;
     }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
+    .badge-all {
+      display: block;
+      letter-spacing: 0.02em;
+      float: left;
+      height: 135px;
+      width: 280px;
+      cursor: pointer;
+      text-decoration: none;
+      position: relative;
+      font-weight: 300;
+      font-size: 12px;
+      line-height: 20px;
+      margin: 0 10px 10px 0;
+      background: transparent;
+      border: 1px solid #fff;
+      border-color: #3e98d3;
+      border-radius: 6px;
     }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
+    .badge-top {
+      background-color: #cfe6f4;
+      border-left: 15px solid #3e98d3;
+      color: #333333;
+      font-weight: 400;
+      height: 2.25em;
+      line-height: 2.25em;
+      padding: 0 0.75em;
     }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
+    .badge-body {
+      border-left: 15px solid #3e98d3;
+      vertical-align: top;
+      padding: 10px 10px;
+      overflow: hidden;
+      position: relative;
+      font-weight: 400;
+      font-size: 12px;
+      margin-bottom: 10px;
     }
-
-    .app-footer a {
-      margin-left: 5px;
+    .badgepic {
+      float: right;
+      width: 60px;
+      height: 60px;
+      display: block;
+      background-size: contain;
+    }
+    .creator {
+      border-left: 15px solid #3e98d3;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      min-height: 30px;
+    }
+    .badge-creator-name {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      margin-bottom: 5px;
+      margin-left: 10px;
+      margin-right: 15px;
+      font-weight: 400;
+      font-size: 13px;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.badgeTopTitle = "Technology & Information";
+    this.badgePic =
+      "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png";
+    this.badgeTitleName = "APA Style Citations: Introduction";
+    this.badgeCreatorName = "Ethan Chen";
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/Project2cool4school.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <div class="badge-all">
+          <div class="badge-top">
+            <span class="badge-top-title">${this.badgeTopTitle}</span>
+          </div>
+          <div class="badge-body">
+            <div class="badge-image">
+              <img class="badgepic" src=${this.badgePic} alt="badge" />
+            </div>
+            <h3>${this.badgeTitleName}</h3>
+          </div>
+          <div class="creator">
+            <div class="badge-creator-name">
+              Creator: ${this.badgeCreatorName}
+            </div>
+          </div>
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
