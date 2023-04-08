beca42e2a544fee257586caafd3fa2336817bbe3
jjw6545@psu.edu <122508759+Jw0001146366@users.noreply.github.com>
2023-04-08 12:47:53 -0400

    Jeff

--- src/search-bar.js
+++ src/search-bar.js
@@ -13,21 +13,45 @@ class Searchbar extends LitElement {
       height: 20px;
       border: 0;
     }
+    .searchInput: {
+      type: String
+    }
   `;
 
+constructor() {
+  super();
+  this.searchInput = "placeholder";    
+}
+  searchInput(e) {
+    this.input = this.shadowRoot.querySelector('input').value;
+  }
+
+  update(changedProperties) {
+    super.update(changedProperties);
+    if (changedProperties.has('searchInput')) {
+      this.dispatchEvent(new CustomEvent('searchChange', {
+        detail: {
+          value: this.searchInput
+        }
+      }));
+    }
+  }
 
   render() {
     return html`
       <main>
         <div class="searchbar">
-          <form>
-            <input
+            <input 
               class="searchbox"
               type="text"
+              id="search"
               placeholder="Search Here Dog"
+              @input="${this.searchInput}"
             />
-          </form>
+          
+          
         </div>
+        
       </main>
     `;
   }
